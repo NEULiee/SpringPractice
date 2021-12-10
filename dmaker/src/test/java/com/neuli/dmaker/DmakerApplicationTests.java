d4e27d220cf1db4a6f50aed2254ccfbb04486674
nueli <asdfz888@naver.com>
2021-12-10 17:13:43 +0900

    dmaker proj 생성

---
+++ dmaker/src/test/java/com/neuli/dmaker/DmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuli.dmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

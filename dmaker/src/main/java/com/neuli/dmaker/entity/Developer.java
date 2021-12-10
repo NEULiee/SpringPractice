86b0a11f46d5c9ed55cb04ba58f87fa94cf6cef8
nueli <asdfz888@naver.com>
2021-12-10 17:38:25 +0900

    H2 DB 생성

---
+++ dmaker/src/main/java/com/neuli/dmaker/entity/Developer.java
@@ -0,0 +1,23 @@
+package com.neuli.dmaker.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    //private DeveloperLebel developerLebel
+}

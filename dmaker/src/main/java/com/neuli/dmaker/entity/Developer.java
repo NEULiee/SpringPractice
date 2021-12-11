b5b80d1242c762cd96d2fd92b88aa18fadc283a5
NEULiee <asdfz888@naver.com>
2021-12-12 00:41:57 +0900

    Entity 생성

--- dmaker/src/main/java/com/neuli/dmaker/entity/Developer.java
+++ dmaker/src/main/java/com/neuli/dmaker/entity/Developer.java
@@ -1,11 +1,15 @@
 package com.neuli.dmaker.entity;
 
+import com.neuli.dmaker.type.DeveloperLevel;
+import com.neuli.dmaker.type.DeveloperSkillType;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -13,11 +17,33 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Developer {
+
+    // Builder 는 NoArgsConstructor, AllArgsConstructor 과 함께 쓰기
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
 
-    //private DeveloperLebel developerLebel
+    @Enumerated(EnumType.STRING)
+    private DeveloperLevel developerLevel;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperSkillType developerSkillType;
+
+    private Integer experienceYears;
+
+    private String memberId;
+
+    private String name;
+
+    private Integer age;
+
+    // row 가 생성되거나 없어질 때 자동으로 값 설정
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }

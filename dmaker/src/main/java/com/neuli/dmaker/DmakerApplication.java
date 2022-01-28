bd48955625f02a779a58c10e82296b3e7283e171
NEULiee <asdfz888@naver.com>
2022-01-28 23:21:56 +0900

    Application @EnableJpaAuditing 설명

--- dmaker/src/main/java/com/neuli/dmaker/DmakerApplication.java
+++ dmaker/src/main/java/com/neuli/dmaker/DmakerApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ *  @EnableJpaAuditing
+ *  : JPA Auditing annotation 을 모두 활성화할 수 있게하는 annotation
+ */
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class DmakerApplication {

2f8a0a0e270fdd931ad8cc62619957d9b7c2b422
NEULiee <asdfz888@naver.com>
2022-01-28 23:19:59 +0900

    JPA Auditing Entity 추가

--- dmaker/src/main/java/com/neuli/dmaker/entity/Developer.java
+++ dmaker/src/main/java/com/neuli/dmaker/entity/Developer.java
@@ -17,10 +17,11 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Developer {
+// @EntityListeners(AuditingEntityListener.class)
+public class Developer extends BaseTimeEntity {
 
     // Builder 는 NoArgsConstructor, AllArgsConstructor 과 함께 쓰기
+
     /**
      * IDENTITY: 기본 키 생성을 데이터베이스에 위임 (AUTO_INCREMENT) ex) MySQL, PostgreSQL, DB2
      * SEQUENCE: 데이터베이스 Sequence Object 를 사용 (DB가 자동으로 숫자를 generate) ex) Oracle, PostgreSQL, DB2, H2
@@ -49,10 +50,20 @@ public class Developer {
     @Enumerated(EnumType.STRING)
     private StatusCode statusCode;
 
-    // row 가 생성되거나 없어질 때 자동으로 값 설정
+    /**
+     *  - JPA auditing: 생성일, 수정일 자동화
+     *  row 가 생성되거나 없어질 때 자동으로 값 설정
+     *
+     *  대부분의 entity 에서 많이 사용하는 값이다.
+     *  따라서 모든 entity 에서 생성일, 수정일을 만들고 수정하려면 코드가 복잡해지고 지저분해진다.
+     *
+     *  아래의 생성일, 수정일을 BaseTimeEntity 를 상속받도록 수정!
+
     @CreatedDate
     private LocalDateTime createdAt;
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+     */
 }

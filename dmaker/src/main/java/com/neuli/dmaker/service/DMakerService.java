638a0657852eb0ca1b3f9144d05a57c925188a0d
NEULiee <asdfz888@naver.com>
2022-01-28 23:20:28 +0900

    Autowired 문제점

--- dmaker/src/main/java/com/neuli/dmaker/service/DMakerService.java
+++ dmaker/src/main/java/com/neuli/dmaker/service/DMakerService.java
@@ -25,14 +25,15 @@ import static com.neuli.dmaker.exception.DMakerErrorCode.*;
 public class DMakerService {
 
     // @RequiredArgsConstructor 필요
+    //
     private final DeveloperRepository developerRepository;
     private final RetiredDeveloperRepository retiredDeveloperRepository;
 
-    // @Autowired, @Inject 문제점
-    // 서비스 코드가 어노테이션에 종속적으로 만들어져
-    // 서비스를 단독으로 테스트 하기가 어려웠기 때문에
-    // 생성자를 생성하는 것이 만들어졌다.
-    // 하지만 생성자도 불편함이 있어 final 을 사용
+    /**
+        [ @Autowired @Inject 문제점 ]
+        서비스 코드가 어노테이션에 종속적으로 만들어 서비스를 단독으로 테스트 하기가 어려웠기 때문에
+        생성자를 생성하는 방법이 생겼지만 생성자도 불편함이 있어 final 을 사용한다.
+    */
 
     /**
      * Transaction

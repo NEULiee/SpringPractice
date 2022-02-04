f8d460f3888a00c0bc3a6b922c4f42ae960c7c9c
NEULiee <asdfz888@naver.com>
2022-02-04 14:22:16 +0900

    @EnableJpaAuditing config 로 변경

--- dmaker/src/main/java/com/neuli/dmaker/DmakerApplication.java
+++ dmaker/src/main/java/com/neuli/dmaker/DmakerApplication.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 /**
  *  @EnableJpaAuditing
  *  : JPA Auditing annotation 을 모두 활성화할 수 있게하는 annotation
+ *
+ *  테스트 도중 bean 이 동작하지 않는 문제가 발생하여 따로 config 파일을 만들어서 등록
  */
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class DmakerApplication {
 

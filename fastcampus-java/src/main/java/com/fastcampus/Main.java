245c0f21238f988d884495f68894378b69b21777
neuli <asdfz888@naver.com>
2022-02-12 14:55:05 +0900

    Implement Main
    메인 , 테스트 구현

--- fastcampus-java/src/main/java/com/fastcampus/Main.java
+++ fastcampus-java/src/main/java/com/fastcampus/Main.java
@@ -19,5 +19,6 @@ public class Main {
     public static void main(String[] args) {
         BubbleSort<String> sort = new BubbleSort<>();
         System.out.println("[result] " + sort.sort(Arrays.asList(args)));
+
     }
 }
